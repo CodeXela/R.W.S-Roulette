86adda93fe048aa74d883a790472b3b580c924df
temako <nobsaibot@gmail.com>
2015-08-30 16:11:40 +0200

    Richtige ausgabe über Main

--- Quelle.cpp
+++ Quelle.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
+#include <string>
 #include <cstdio>
 #include <cstdlib>
 #include <stdio.h>
+#include <Windows.h>
 
 #include "Tisch.h"
 
 using namespace std;
 
 
+	
+
+
 int main()
 {
+	
 
 	Tisch T1;
+	system("CLS");
+	T1.Print();
+	
 
 
 
 	getchar();
 	return 0;
 }
+
