aaf8ba94864d739ce641ec91632e033755e3ceb1
temako <nobsaibot@gmail.com>
2015-08-30 16:10:04 +0200

    Fenstergröße und Farben

--- Tisch.cpp
+++ Tisch.cpp
@@ -1,26 +1,87 @@
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <stdio.h>
+#include <Windows.h>
 #include "Tisch.h"
 
+using namespace std;
 
+enum Colors { blue = 1, green, cyan, red, purple, yellow, grey, dgrey, hblue, hgreen, hred, hpurple, hyellow, hwhite };// vergebe feste Nummern für Farben
 
 
 Tisch::Tisch()
 {
-	InitTisch(false,false,false);
-	_spieler.initSpieler("Alex",100.0f,0.0f,0.0f,0);
-	_casinoBank.initCB(1000000.0f);
+	
+	SetWindow(85,40);//setze Consolengröße fest
+
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), BACKGROUND_GREEN);//setze Consolenhintergrund fest 
+
+	InitTisch(false,false,false,0.0f,0.0f,0);//Statistik startwerte (MERKE*ersetzen mit eingabe)
+	
+	_spieler.initSpieler("Temako",100.0f);// Spieler startwerte (MERKE*ersetzen mit eingabe)
+
+	_casinoBank.initCB(1000000.0f);// Casinobank wird aufgefüllt(MERKE*ersetzen mit eingabe)
+	
 }
 
 Tisch::~Tisch()
 {
 
 }
-void Tisch::InitTisch(bool rundeGewonnen, bool bleibtRot, bool bleibtSchwarz)
+void Tisch::InitTisch(bool rundeGewonnen, bool bleibtRot, bool bleibtSchwarz, float verlorenAufRot, float verlorenAufSchwarz, int gespielteSpiele)
 {
 
 	_RundeGewonnen = rundeGewonnen;
 	_bleibtRot = bleibtRot;
 	_bleibtSchwarz = bleibtSchwarz;
+	_verlorenAufRot = verlorenAufRot;
+	_verlorenAufSchwarz = verlorenAufSchwarz;
+	_gespielteSpiele = gespielteSpiele;
 
 }
+
+void Tisch::Print()
+{
+	
+	
+	printf("                                                                        \n");
+	
+
+	cout <<"Spieler Name:   "<<_spieler.holeSpielerName(); printf("                             Casino Bank:  %0.2f Euro\n", _casinoBank.BankPrint());
+
+	printf("Spieler Konto:  %0.2f Euro", _spieler.holeSpielerKonto()); printf("                         Mindesteinsatz:  %0.2f Cent\n", _Mindesteinsatz);
+
+	
+
+
+}
+void Tisch::SetWindow(int Width, int Height) // Aufbau der consolengröße
+{
+	_COORD coord;
+	coord.X = Width;
+	coord.Y = Height;
+
+	_SMALL_RECT Rect;
+	Rect.Top = 0;
+	Rect.Left = 0;
+	Rect.Bottom = Height - 1;
+	Rect.Right = Width - 1;
+
+	HANDLE Handle = GetStdHandle(STD_OUTPUT_HANDLE);      // Get Handle
+	SetConsoleScreenBufferSize(Handle, coord);            // Setze Buffer Größe
+	SetConsoleWindowInfo(Handle, TRUE, &Rect);            // Setze Window Größe
+	
+	
+}
+void Tisch::coutc(int color, char* output)
+{
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(handle, color);
+	cout << output;
+	SetConsoleTextAttribute(handle, color);
+}
+
 
 
